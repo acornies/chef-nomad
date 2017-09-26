ca2314af9f674c0da4844064eed738bc5a115202
Andrew Cornies <andrew.cornies@rakuten.com>
2017-09-26 08:04:19 -0400

    bumping to 0.7.0-beta1

--- attributes/install.rb
+++ attributes/install.rb
@@ -32,8 +32,8 @@ default['nomad'].tap do |nomad|
       )
     else
       %w(
-        0.6.2/nomad_0.6.2_linux_amd64.zip
-        fbcb19a848fab36e86ed91bb66a1602cdff5ea7074a6d00162b96103185827b4
+        0.7.0-beta1/nomad_0.7.0-beta1_linux_amd64.zip
+        174794d96d2617252875e2e2ff9e496120acc4a97be54965c324b9a5d11b37ab
       )
     end
 end
